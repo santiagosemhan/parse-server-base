295792763b97e5d1da83dbf0f2dc0cd6d3c825bb
Santiago Semhan <santiagosemhan@gmail.com>
2020-06-11 17:31:30 -0300

    Changes on env vars.

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { nullParser, booleanParser } from './cloud/utils';
 import SimpleSendGridAdapter from './adapters/SendGridAdapter';
 
 const port = process.env.PORT || 4444;
-const serverURL = process.env.CORE_URL || `http://localhost:${port}/parse`;
+const serverURL = process.env.PARSE_SERVER_URL || `http://localhost:${port}/parse`;
 const publicServerURL = process.env.PUBLIC_SERVER_URL || serverURL;
 const verifyUserEmails = booleanParser(process.env.VERIFY_USER_EMAILS) || false;
 const preventLoginWithUnverifiedEmail =
@@ -15,7 +15,7 @@ export default {
   databaseURI: process.env.MONGO_DSN, // Connection string for your MongoDB database
   appId: process.env.APP_ID,
   masterKey: process.env.PARSE_SERVER_MASTER_KEY, // Keep this key secret!
-  readOnlyMasterKey: process.env.READ_ONLY_MASTER_KEY,
+  readOnlyMasterKey: process.env.PARSE_SERVER_READ_ONLY_MASTER_KEY,
   logsFolder: nullParser(process.env.PARSE_SERVER_LOGS_FOLDER),
   // logLevel: 'error',
   cloud: `${__dirname}/cloud/`, // Absolute path to your Cloud Code
